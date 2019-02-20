ca4e41d6c6d7b2aeb6aa85d4436c681a29f52978
Kolos <kolos181@gmail.com>
2019-02-20 11:52:40 +0200

    Added new entities and established connections between them

--- src/main/java/guru/springframework/recipeproject/domain/Recipe.java
+++ src/main/java/guru/springframework/recipeproject/domain/Recipe.java
@@ -1,6 +1,7 @@
 package guru.springframework.recipeproject.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -19,11 +20,17 @@ public class Recipe {
     //todo add
     // private Difficulty difficulty;
 
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
+
     @Lob
     private Byte[] image;
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+
+    @OneToOne
+    private UnitOfMeasure unitOfMeasure;
 
     public Long getId() {
         return id;
@@ -103,5 +110,21 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public UnitOfMeasure getUnitOfMeasure() {
+        return unitOfMeasure;
+    }
+
+    public void setUnitOfMeasure(UnitOfMeasure unitOfMeasure) {
+        this.unitOfMeasure = unitOfMeasure;
     }
 }
