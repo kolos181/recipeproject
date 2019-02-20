45b4ce29e3df102ca8e1108dc266380068f16bd6
Kolos <kolos181@gmail.com>
2019-02-20 13:23:46 +0200

    added Category, Recipe and UOM repositories

--- src/main/java/guru/springframework/recipeproject/domain/Difficulty.java
+++ src/main/java/guru/springframework/recipeproject/domain/Difficulty.java
@@ -1,5 +1,7 @@
 package guru.springframework.recipeproject.domain;
 
+import javax.persistence.Entity;
+
 public enum Difficulty {
 
     EASY, MODERATE, HARD
--- src/main/java/guru/springframework/recipeproject/domain/Notes.java
+++ src/main/java/guru/springframework/recipeproject/domain/Notes.java
@@ -2,6 +2,7 @@ package guru.springframework.recipeproject.domain;
 
 import javax.persistence.*;
 
+@Entity
 public class Notes {
 
     @Id
